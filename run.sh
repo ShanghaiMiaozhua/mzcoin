a63781bab284259905fb0c4f2c04b02bdd98a3e1
iketheadore <heaven.mao@gmail.com>
2016-12-20 18:30:06 +0800

    Clean up comments

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "mzcoin binary dir:" "$DIR"
 
 pushd "$DIR" >/dev/null
-#./scripts/clean-static-libs.sh >/dev/null 2>&1
+
 go run cmd/mzcoin/mzcoin.go --gui-dir="${DIR}/src/gui/static/" $@
 
 popd >/dev/null
